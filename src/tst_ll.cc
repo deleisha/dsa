eac202654906136e4aad7a8dc9218afad1bf70ac
Devchandra Leishangthem <dlmeetei@gmail.com>
2015-11-24 01:53:39 +0530

    rearrange test code for better testability

--- src/tst_ll.cc
+++ src/tst_ll.cc
@@ -7,33 +7,40 @@ typedef struct my_list {
     sl_list_t nd;
 } test_list;
 
-int main()
+int main(int argc, char *argv[])
 {
     SL_LIST_HD(hdr);
 
-    test_list first = { .value = 10, .nd = { .nxt = NULL }};
-    test_list second = { .value = 20, .nd = { .nxt = NULL }};
-    test_list third = { .value = 30, .nd = { .nxt = NULL }};
+    test_list element[] = {
+        { .value = 10, .nd = { .nxt = NULL }},
+        { .value = 20, .nd = { .nxt = NULL }},
+        { .value = 30, .nd = { .nxt = NULL }}
+    };
 
-    insert(&(first.nd), &hdr);
-    insert(&(second.nd), &hdr);
-    append(&(third.nd), &hdr);
+    //test adding node
+    insert(&(element[0].nd), &hdr);
+    append(&(element[1].nd), &hdr);
+    append(&(element[2].nd), &hdr);
 
+    //test traversing
+    int i = 0;
     for( sl_list_t *n = hdr.nxt; n; n = n->nxt) {
         test_list *nd = SL_LIST_ENTRY(n, test_list, nd);
-        std::cout << nd->value << std::endl;
+        assert( nd == &(element[i]));
+        ++i;
     }
 
     assert( 3 == sllist_len(hdr.nxt));
 
     //test delete node operations
-    delete_nd( &(third.nd), &hdr);
+    delete_nd( &(element[2].nd), &hdr);
     assert( 2 == sllist_len(hdr.nxt));
 
-    for( sl_list_t *n = hdr.nxt; n; n = n->nxt) {
-        test_list *nd = SL_LIST_ENTRY(n, test_list, nd);
-        std::cout << nd->value << std::endl;
-    }
+    insert(&(element[2].nd), &hdr);
+    assert( 3 == sllist_len(hdr.nxt));
 
+    delete_nd( &(element[0].nd), &hdr);
+    assert( 2 == sllist_len(hdr.nxt));
 
+    std::cout << "Test: " << argv[0] << ": +++++++ passed +++++++" << std::endl;
 }
