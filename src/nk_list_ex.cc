0f4b8fa2bead1d9cd4745bd87defd8893fd193af
Devchandra Leishangthem <dlmeetei@gmail.com>
2015-11-27 10:13:36 +0530

    circular algo added

--- src/nk_list_ex.cc
+++ src/nk_list_ex.cc
@@ -36,8 +36,8 @@ my_list* nth_from_end(sl_list_t *list, const int n)
         }
         assert(itr != NULL);
         itr = itr->nxt;
-
     }
+
     for(rptr = list; itr; itr = itr->nxt)
     {
         rptr = rptr->nxt;
@@ -45,19 +45,70 @@ my_list* nth_from_end(sl_list_t *list, const int n)
     return SL_LIST_ENTRY(rptr, my_list, nd);
 }
 
-int main(int argc, char *argv[])
+bool is_circular(sl_list_t *list)
 {
-    SL_LIST_HD(hdr);
-    for (int i = 0; i < ARRAY_SIZE(nodes); ++i) {
-        insert(&(nodes[ARRAY_SIZE(nodes) - (i + 1)].nd) , &hdr);
+    sl_list_t *slow_ptr = NULL;
+    sl_list_t *fast_ptr = NULL;
+    for(slow_ptr = list, fast_ptr = list; slow_ptr && fast_ptr && fast_ptr->nxt;
+           slow_ptr = slow_ptr->nxt, fast_ptr = fast_ptr->nxt)
+    {
+        fast_ptr = fast_ptr->nxt;
+        if ( slow_ptr == fast_ptr ) return true;
     }
+    return false;
+}
 
-    sl_list_t *ptr = NULL;
-    SL_FOR_EACH(ptr, hdr.nxt) {
-        my_list  *p = SL_LIST_ENTRY(ptr, my_list, nd);
-        std::cout << p->value << std::endl;
+sl_list_t* find_loop_start(sl_list_t *list)
+{
+    sl_list_t *slow_ptr = NULL;
+    sl_list_t *fast_ptr = NULL;
+    bool is_circular = false;
+    for(slow_ptr = list, fast_ptr = list; slow_ptr && fast_ptr && fast_ptr->nxt;
+           fast_ptr = fast_ptr->nxt, slow_ptr = slow_ptr->nxt)
+    {
+        fast_ptr = fast_ptr->nxt;
+        if ( slow_ptr == fast_ptr ) {
+            is_circular = true;
+            break;
+        }
     }
+    if (is_circular) {
+        for( fast_ptr = list; slow_ptr != fast_ptr; slow_ptr = slow_ptr->nxt) {
+     //       std::cout << (SL_LIST_ENTRY(slow_ptr, my_list, nd))->value << "\t" << (SL_LIST_ENTRY(fast_ptr, my_list, nd))->value << std::endl;
+            fast_ptr = fast_ptr->nxt;
+        }
 
+        return slow_ptr;
+    }
+    return NULL;
+}
+int find_loop_len(sl_list_t *list)
+{
+    sl_list_t *slow_ptr = NULL;
+    sl_list_t *fast_ptr = NULL;
+    bool is_circular = false;
+    for(slow_ptr = list, fast_ptr = list; slow_ptr && fast_ptr && fast_ptr->nxt;
+           slow_ptr = slow_ptr->nxt, fast_ptr = fast_ptr->nxt)
+    {
+        fast_ptr = fast_ptr->nxt;
+        if ( slow_ptr == fast_ptr ) {
+            is_circular = true;
+            break;
+        }
+    }
+    int cnt = 0;
+    if (is_circular) {
+        for( ;slow_ptr != fast_ptr; fast_ptr = fast_ptr->nxt)
+        {
+            ++cnt;
+        }
+    }
+    return cnt;
+}
+
+
+void test_nth_from_end(sl_list_t hdr)
+{
     my_list *t = nth_from_end(hdr.nxt, 5);
     std::cout << t->value << std::endl;
 
@@ -66,4 +117,39 @@ int main(int argc, char *argv[])
 
     t = nth_from_end(hdr.nxt, 15);
     assert( t == NULL);
+}
+
+#define ENABLE_DEBG 1
+int main(int argc, char *argv[])
+{
+    SL_LIST_HD(hdr);
+    for (int i = 0; i < ARRAY_SIZE(nodes); ++i) {
+        insert(&(nodes[ARRAY_SIZE(nodes) - (i + 1)].nd) , &hdr);
+    }
+
+#if ENABLE_DEBG
+    sl_list_t *ptr = NULL;
+    SL_FOR_EACH(ptr, hdr.nxt) {
+        my_list  *p = SL_LIST_ENTRY(ptr, my_list, nd);
+        std::cout << p->value << std::endl;
+    }
+#endif
+//    test_nth_from_end( hdr);
+
+    //test floyd algo
+    nodes[9].nd.nxt = nodes[4].nd.nxt;
+
+    assert(is_circular(hdr.nxt));
+//    std::cout << (SL_LIST_ENTRY(find_loop_start(hdr.nxt), my_list, nd))->value << std::endl;
+#if ENABLE_DEBG
+    SL_FOR_EACH(ptr, hdr.nxt) {
+        my_list  *p = SL_LIST_ENTRY(ptr, my_list, nd);
+        std::cout << p->value << std::endl;
+    }
+#endif
+
+    nodes[9].nd.nxt = NULL;
+    assert(!is_circular(hdr.nxt));
+    nodes[9].nd.nxt = nodes[0].nd.nxt;
+    assert(is_circular(hdr.nxt));
 }
